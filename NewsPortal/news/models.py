32a2432fe5b57683b67aeba55bcdfb172e9a62ad
Oleg Riabtsev <Oleg.Ryabtsev@unit4.com>
2023-05-26 12:32:46 +0200

    Created django project NewsPortal

---
+++ NewsPortal/news/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_comments_rating = self.posts.aggregate(models.Sum('comments__rating')).get('comments__rating__sum') or 0
+        self.rating = (self.posts.aggregate(models.Sum('rating')).get('rating__sum') or 0) * 3
+        self.rating += post_comments_rating
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    POST_TYPES = [
+        ('Article', 'Article'),
+        ('News', 'News'),
+    ]
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    post_type = models.CharField(max_length=10, choices=POST_TYPES)
+    created_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...' if len(self.text) > 124 else self.text
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
