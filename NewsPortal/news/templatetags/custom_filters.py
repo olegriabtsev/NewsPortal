b0d231b9ef963028487f8b8ee25f1c14d792a36d
Oleg Riabtsev <lehaspost@gmail.com>
2023-10-04 17:50:29 +0200

    Finished Итоговое задание 6.6 (HW-03)

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.filter(name='censor')
 def censor(value):
     # Заменяем нежелательные слова на '*'
-    words_to_censor = ['нежелательное1', 'нежелательное2']  # Замените на реальные нежелательные слова
+    words_to_censor = ['пипец', 'зашибись']  # Замените на реальные нежелательные слова
     for word in words_to_censor:
         value = value.replace(word, '*' * len(word))
     return value
